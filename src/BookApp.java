6d8946e8b69463bec9262edd304d81ccb37b563a
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-17 13:58:43 -0400

    new method

--- src/BookApp.java
+++ src/BookApp.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class BookApp {
 
     public static void main(String[] args) {
 
        Book one = new Book();
+       int number;
 
        one.setAuthor("N.T. Wright");
        one.setBookTitle("justification");
        one.setDescription("New Perspective");
+       one.setPrice(5);
+       one.setInStock(true);
 
-//        System.out.println();
         one.displayText();
+
+        System.out.println("how many books are present?");
+
+
+        Scanner key = new Scanner(System.in);
+        number = key.nextInt();
+
+
+        System.out.println(one.displayCost(number));
+
+
+
+
+
+
+
+
 
 
 
